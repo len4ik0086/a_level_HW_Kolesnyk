ccaeabadfc59c952fe7915669955600c26cb2084
Olena Kolesnyk <len4ik0086@gmail.com>
2024-01-02 21:55:22 +0100

    Added Homework #1

---
+++ src/main/java/Task1_2.java
@@ -0,0 +1,9 @@
+public class Task1_2 {
+    public static void main(String[] args) {
+        String name = "Olena";
+        int height = 168;
+        double weight = 60.5;
+
+        System.out.println("Ім'я=" + name + ", Зріст=" + height + ", Вага="+weight);
+    }
+}
