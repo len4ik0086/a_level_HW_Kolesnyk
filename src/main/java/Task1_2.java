e9755ba68b7ee33ccb4ad4532f4b040c84fec080
Olena Kolesnyk <len4ik0086@gmail.com>
2024-01-12 12:29:31 +0100

    deleted as renamed according to new rules

--- src/main/java/Task1_2.java
+++
@@ -1,9 +0,0 @@
-public class Task1_2 {
-    public static void main(String[] args) {
-        String name = "Olena";
-        int height = 168;
-        double weight = 60.5;
-
-        System.out.println("Ім'я=" + name + ", Зріст=" + height + ", Вага="+weight);
-    }
-}
