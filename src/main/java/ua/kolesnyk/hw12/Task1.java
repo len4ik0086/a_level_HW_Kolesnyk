8670600b1371165fd27d62012481ab5d30568758
Olena Kolesnyk <len4ik0086@gmail.com>
2024-02-11 12:26:02 +0100

    HW 12 for check

---
+++ src/main/java/ua/kolesnyk/hw12/Task1.java
@@ -0,0 +1,52 @@
+package ua.kolesnyk.hw12;
+
+//Реализовать программу которая считывает текст с консоли и строит CSV файл.
+//● Если пользователь ввел next то происходит переход строки
+//● Если пользователь вводит end то программа завершается
+//Пример ввода:
+//hi there
+//world
+//next
+//hello
+//next
+//jesus
+//end
+//Пример результата:
+//hi there, world,
+//hello,
+//jesus
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.csv"));
+
+            System.out.println("Введите текст: \n" + "● Если ввели next, то происходит переход строки \n" +
+                    "● Если ввели end, то программа завершается");
+
+            String line;
+            while (!(line = scanner.nextLine()).equals("end")) {
+                if (line.equals("next")) {
+                    writer.newLine();
+                } else {
+                    writer.write(line);
+                    if (!line.isEmpty()) {
+                        writer.write(", ");
+                    }
+                }
+            }
+
+            scanner.close();
+            writer.close();
+            System.out.println("CSV файл создан.");
+        } catch (IOException e) {
+            System.out.println("Ошибка создания CSV файла: " + e.getMessage());
+        }
+    }
+}
